aa0668b125ece210c22f8917a1c20ad471774262
GuilhermeGaffuri <150754130+GuilhermeGaffuri@users.noreply.github.com>
2024-04-30 20:56:27 -0300

    Add files via upload

---
+++ Calculadora.js
@@ -0,0 +1,23 @@
+function soma (n1,n2){
+
+    return n1 + n2
+}
+function div (n1,n2){
+
+    return n1 / n2
+}
+function mult (n1,n2){
+
+    return n1 * n2
+}
+function sub (n1,n2){
+
+    return n1 - n2
+}
+
+module.exports = {
+    soma, 
+    div, 
+    mult, 
+    sub
+}
---
+++ Index.js
@@ -0,0 +1,11 @@
+const {soma} = require('./Calculadora');
+const {mult} = require('./Calculadora');
+const {div} = require('./Calculadora');
+const {sub} = require('./Calculadora');
+
+console.log(soma(1,2));
+console.log(mult(1,2));
+console.log(div(6,2));
+console.log(sub(7,1));
+
+console.log("----------------------------------------------");
